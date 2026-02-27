8c67fcb8195c49690073ccdc2080b03e21be97e0
Gilang737 <gs7832583@gmail.com>
2026-02-27 14:42:23 +0700

    update lagi

--- js/script.js
+++ js/script.js
@@ -218,3 +218,35 @@ document.addEventListener('DOMContentLoaded', () => {
     initScrollAnimations();
     initSmoothScroll();
 });
+
+var form = document.getElementById("form");
+  
+  async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    var data = new FormData(event.target);
+
+    fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+          'Accept': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        window.location.href = "../thanks.html"; 
+      } else {
+        response.json().then(data => {
+          if (Object.hasOwn(data, 'errors')) {
+            status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
+          } else {
+            status.innerHTML = "Ada masalah saat mengirim.";
+          }
+        })
+      }
+    }).catch(error => {
+      status.innerHTML = "Sepertinya ada gangguan koneksi.";
+    });
+  }
+
+  form.addEventListener("submit", handleSubmit);
